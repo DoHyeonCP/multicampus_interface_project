f379858ba19e702fc258fcad45d378ed9b09ece2
JOO-WON-JANG <joowon5004@naver.com>
2023-03-13 00:52:12 +0900

    인증기능 add

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,7 @@
 """config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from streaming import views
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'common'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('common/',include('common.urls')), #JOOWON: 인증요청
-    # path('streaming/', views.index)
+    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
