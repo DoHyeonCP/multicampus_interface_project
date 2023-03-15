25390a0ff0fac360f30aa38bf737b860f70897fd
Vineryd <starlmylove@gmail.com>
2023-03-15 17:02:35 +0900

    좋아요 기능 추가 어떤 템플릿에 추가해야하나요

--- streaming/views.py
+++ streaming/views.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q, Count
 from django.db import models
+from .models import Like
 
 def mainpage(request):
     return render(request, 'Landingpage.html')
@@ -14,8 +15,17 @@ class Post(models.Model):
     content = models.TextField()
     views = models.PositiveBigIntegerField(default=0)
 
-def post_detail(request, pk):
+def post_m(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.views += 1
     post.save()
-    return render(request, '#.html', {'post':post})
+    return render(request, 'video.html', {'post':post})
+
+def post_l(request, pk):
+    post = get_object_or_404(Like, pk=pk)
+    user = request.user
+    if user in post.likes.all():
+        post.likes.remove(user)
+    else:
+        post.likes.add(user)
+    return redirect('video.html', post_id=post_id)
