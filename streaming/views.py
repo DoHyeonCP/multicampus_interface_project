3cc8cb7778c1c2fdddd52e92847e7805ad0a5843
Vineryd <starlmylove@gmail.com>
2023-03-16 10:24:38 +0900

    구독 기능 추가

--- streaming/views.py
+++ streaming/views.py
@@ -1,8 +1,22 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Q, Count
+from django.contrib.auth.decorators import login_required
 from django.db import models
-from .models import Like
+from .models import Like, Subscription
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    views = models.PositiveBigIntegerField(default=0)
+@login_required(login_url = '')
+def subscribe(request, pk):
+    item = 모델.objects.get(pk=pk)
+    if Subscription.objects.filter(user = request.user, subscribed_to=item).exists():
+        #이미 구독중이라면
+        return redirect('모델의 뷰', pk=pk)
+    Subscription.objects.create(user=request.user, subscribed_to=item)
+    return redirect('모델의 뷰', pk=pk)
 
 def mainpage(request):
     return render(request, 'Landingpage.html')
@@ -10,10 +24,7 @@ def mainpage(request):
 def auth(request):
     return render(request, 'auth.html')
 
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    views = models.PositiveBigIntegerField(default=0)
+
 
 def post_m(request, pk):
     post = get_object_or_404(Post, pk=pk)
